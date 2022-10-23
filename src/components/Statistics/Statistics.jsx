76da817c892d97771f316f834e314e618ba1dcc5
Victor Nikolenko <victor1982nik@gmail.com>
2022-10-23 22:43:26 +0300

    исправления замечаний ментора

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,7 +5,7 @@ import css from './Statistics.module.css';
 export function Statistics({ title, stats }) {
             
     return <section className={css.statistics}>
-        <h2 className={css.title}>{title ?? ""}</h2>
+        {title && <h2 className={css.title}>{title}</h2>}
 
         <ul className={css.stat_list} >            
             {stats.map(({ label, percentage, id }) => {
